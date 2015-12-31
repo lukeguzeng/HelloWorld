f4d03da30f52a29e48be69fe15c71571062ea51d
maxguzeng <263457839@qq.com>
2015-12-31 19:04:21 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        user.firstName = "Steven";
+        user.lastName = "Liao";
+
+        System.out.println("Hello! " + user.firstName + " " + user.lastName);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 12/31/2015.
+ */
+public class User {
+    public String firstName;
+    public String lastName;
+    public int phoneNumber;
+}
